e2c963e8a5c48120b25e998f4265d34d22f9ed40
Martin Ficzel <ficzel@sitegeist.de>
2021-10-05 19:32:07 +0200

    TASK: Use PSR7  and flow httpFactories
    This avoids the direct dependency to guzzle but still uses psr factories and requests as much as possible.
    Only the http timeout is set as curl option.

--- Classes/Domain/Service/DeepLService.php
+++ Classes/Domain/Service/DeepLService.php
@@ -3,6 +3,11 @@
 namespace CodeQ\DeepLTranslationHelper\Domain\Service;
 
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Http\Client\Browser;
+use Neos\Flow\Http\Client\CurlEngine;
+use Neos\Http\Factories\ServerRequestFactory;
+use Neos\Http\Factories\StreamFactory;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Log\LoggerInterface;
 
 /**
@@ -24,6 +29,18 @@ class DeepLService
     protected $logger;
 
     /**
+     * @Flow\Inject
+     * @var ServerRequestFactory
+     */
+    protected $serverRequestFactory;
+
+    /**
+     * @Flow\Inject
+     * @var StreamFactory
+     */
+    protected $streamFactory;
+
+    /**
      * @param string[] $texts
      * @param string $targetLanguage
      * @param string|null $sourceLanguage
@@ -31,78 +48,67 @@ class DeepLService
      */
     public function translate(array $texts, string $targetLanguage, ?string $sourceLanguage = null): array
     {
+        // store keys and values seperately for later reunion
         $keys = array_keys($texts);
         $values = array_values($texts);
 
         $baseUri = $this->settings['useFreeApi'] ? $this->settings['baseUriFree'] : $this->settings['baseUri'];
 
-        $curlHandle = curl_init($baseUri . 'translate');
-        curl_setopt($curlHandle, CURLOPT_TIMEOUT, 0);
-        curl_setopt($curlHandle, CURLOPT_HTTPHEADER, ['Expect:']);
-        curl_setopt($curlHandle, CURLOPT_POST, true);
-        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curlHandle, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-        curl_setopt($curlHandle, CURLOPT_HTTPHEADER, [
-            'Accept: */*',
-            'Content-Type: application/x-www-form-urlencoded',
-            sprintf('Authorization: DeepL-Auth-Key %s', $this->settings['apiAuthKey'])
-        ]);
-
-        // create request body ... neither psr nor guzzle can create the body format that
-        // is required here
+        // request body ... this has to be done manually because of the non php ish format
+        // with multiple text arguments
         $body = http_build_query($this->settings['defaultOptions']);
         if ($sourceLanguage) {
             $body .= '&source_lang=' . urlencode($sourceLanguage);
         }
         $body .= '&target_lang=' . urlencode($targetLanguage);
-
         foreach($values as $part) {
             $body .= '&text=' . urlencode($part);
         }
 
-        curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $body);
+        $apiRequest = $this->serverRequestFactory->createServerRequest('POST', $baseUri . 'translate')
+            ->withHeader('Accept', 'application/json')
+            ->withHeader('Authorization', sprintf('DeepL-Auth-Key %s', $this->settings['apiAuthKey']))
+            ->withHeader('Content-Type', 'application/x-www-form-urlencoded')
+            ->withBody($this->streamFactory->createStream($body));
 
-        // return
-        $curlResult = curl_exec($curlHandle);
-        if ($curlResult === false) {
-            return $texts;
-        }
+        $browser = new Browser();
+        $engine = new CurlEngine();
+        $engine->setOption(CURLOPT_TIMEOUT, 0);
+        $browser->setRequestEngine($engine);
 
-        $status = curl_getinfo($curlHandle, CURLINFO_RESPONSE_CODE);
+        /**
+         * @var ResponseInterface $apiResponse
+         */
+        $apiResponse = $browser->sendRequest($apiRequest);
 
-        if ($status != 200) {
-            if ($status === 403) {
+        if ($apiResponse->getStatusCode() == 200) {
+            $returnedData = json_decode($apiResponse->getBody()->getContents(), true);
+            if (is_null($returnedData)) {
+                return $texts;
+            }
+            $translations = array_map(
+                function($part) {
+                    return $part['text'];
+                },
+                $returnedData['translations']
+            );
+            return array_combine($keys, $translations);
+        } else {
+            if ($apiResponse->getStatusCode() === 403) {
                 $this->logger->critical('Your DeepL API credentials are either wrong, or you don\'t have access to the requested API.');
-            } elseif ($status === 429) {
+            } elseif ($apiResponse->getStatusCode() === 429) {
                 $this->logger->warning('You sent too many requests to the DeepL API, we\'ll retry to connect to the API on the next request');
-            } elseif ($status === 456) {
+            } elseif ($apiResponse->getStatusCode() === 456) {
                 $this->logger->warning('You reached your DeepL API character limit. Upgrade your plan or wait until your quota is filled up again.');
-            } elseif ($status === 400) {
+            } elseif ($apiResponse->getStatusCode() === 400) {
                 $this->logger->warning('Your DeepL API request was not well-formed. Please check the source and the target language in particular.', [
                     'sourceLanguage' => $sourceLanguage,
                     'targetLanguage' => $targetLanguage
                 ]);
             } else {
-                $this->logger->warning('Unexpected status from Deepl API', ['status' => $status]);
+                $this->logger->warning('Unexpected status from Deepl API', ['status' => $apiResponse->getStatusCode()]);
             }
             return $texts;
         }
-
-        curl_close($curlHandle);
-
-        $returnedData = json_decode($curlResult, true);
-
-        if (is_null($returnedData)) {
-            return $texts;
-        }
-
-        $translations = array_map(
-            function($part) {
-                return $part['text'];
-            },
-            $returnedData['translations']
-        );
-
-        return array_combine($keys, $translations);
     }
 }
